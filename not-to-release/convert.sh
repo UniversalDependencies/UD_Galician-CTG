f4c8f71015ecf6adc0d021c0e1308a924eebf88f
Martin Popel <popel@ufal.mff.cuni.cz>
2017-06-22 17:30:00 +0200

    use ud.ComplyWithText to match the annotation with the raw texts

--- not-to-release/convert.sh
+++ not-to-release/convert.sh
@@ -3,8 +3,14 @@
 udapy -h >/dev/null || { echo "udapy is not installed, see https://github.com/udapi/udapi-python"; exit 1; }
 
 for a in train dev test; do
-    cat gl-ud-$a.conllu | udapy -s ud.SetSpaceAfter ud.gl.To2 ud.Convert1to2 util.Eval \
-        tree="tree.bundle.bundle_id='$a-'+tree.bundle.bundle_id" \
+    cat gl-ud-$a.conllu | udapy -s \
+      read.Conllu \
+      read.AddSentences files=raw_$a.txt \
+      ud.ComplyWithText \
+      ud.JoinAsMwt \
+      ud.gl.To2 \
+      ud.Convert1to2 \
+      util.Eval tree="tree.bundle.bundle_id='$a-'+tree.bundle.bundle_id" \
     > ../gl-ud-$a.conllu
 done
 
